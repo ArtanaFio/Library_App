eaef9b7790d317deca3f607f2c3ab80ed0dce414
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-04-29 14:39:35 -0400

    Create book catalog modal
    Developed the catalog interface and programmed the script to open the
    catalog submit form when the user selects the catalog, show the submit
    message when the user selects the submit button, and close the form when
    the user selects the close button.

--- Assets/JS/library.js
+++ Assets/JS/library.js
@@ -5,6 +5,8 @@ const myLibrary = [];
 
 // Function to create a "book" object using an object constructor
 function Book(title, author, pages, read) {
+
+    // "book" constructor
     this.title = title; // enter a string representing the book's title
     this.author = author; // enter a string representing the author's name
     this.pages = pages; // enter a number representing the number of pages within the book
@@ -27,4 +29,35 @@ function addBookToLibrary() {
 
     // myLibrary.push(BookNickname);
 }
+
+// Open the book catalog submit form
+
+const bookCatalog = document.querySelector(".catalog");
+const bookModal = document.getElementById("book-modal");
+
+bookCatalog.addEventListener('click', () => {
+    bookModal.style.display = "flex";
+    /*let newBook = document.createElement('div');
+    let newTitle = document.createElement('p');
+    newBook.classList.add('book');
+    newTitle.classList.add('title');*/
+});
+
+// Show submit message
+
+const submitButton = document.querySelector(".submit-button");
+const submitMessage = document.querySelector(".message");
+
+submitButton.addEventListener('click', () => {
+    submitMessage.textContent = "You have added a book to the library!";
+});
+
+// Close the book modal
+
+const closeButton = document.querySelector(".close");
+
+closeButton.addEventListener('click', () => {
+    bookModal.style.display = "none";
+    submitMessage.textContent = "";
+})
 
