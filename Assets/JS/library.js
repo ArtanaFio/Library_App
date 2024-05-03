ee6774f5d1d053c05b398a9b38efd01dd706bc28
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-05-03 19:58:35 -0400

    Fix form validation and add book to user interface
    Created input:invalid CSS class to highlight empty inputs for user.
    Updated the JavaScript code to show appropriate message for user when
    filing out the form and to create a new "book" object from input values
    and to create a visual representation of the book on the user interface
    inside of the bookcase. Toggled the "bookcase" div element's visibility
    using event listeners on the book catalog submit form.

--- Assets/JS/library.js
+++ Assets/JS/library.js
@@ -12,21 +12,12 @@ const bookTitleInput = document.getElementById("book-title");
 const bookAuthorInput = document.getElementById("book-author");
 const bookPagesInput = document.getElementById("book-pages");
 const bookGenreInput = document.getElementById("book-genre");
+const bookcase = document.getElementById("storage");
+const firstShelf = document.querySelector(".first");
+const secondShelf = document.querySelector(".second");
+const thirdShelf = document.querySelector(".third");
+const fourthShelf = document.querySelector(".fourth");
 
-// Open the book catalog submit form
-bookCatalog.addEventListener('click', () => {
-    bookModal.style.display = "flex";
-    /*let newBook = document.createElement('div');
-    let newTitle = document.createElement('p');
-    newBook.classList.add('book');
-    newTitle.classList.add('title');*/
-});
-
-// Close the book modal
-closeButton.addEventListener('click', () => {
-    bookModal.style.display = "none";
-    submitMessage.textContent = "";
-})
 
 // Function to create a "book" object using an object constructor
 function Book(title, author, pages, genre) {
@@ -41,50 +32,52 @@ function Book(title, author, pages, genre) {
     };
 }
 
-
 // Function to add the "book" objects to the "myLibrary" array
 function addBookToLibrary() {
 
     submitButton.addEventListener('click', () => {
-
+        
         const bookTitle = bookTitleInput.value;
         const bookAuthor = bookAuthorInput.value;
         const bookPages = bookPagesInput.value;
         const bookGenre =bookGenreInput.value;
 
-        // Creates a book object from user input
-        const libraryBook = new Book(bookTitle, bookAuthor, bookPages, bookGenre);
-
-        // Show submit message
-        submitMessage.textContent = "You have added a book to the library!";
-
-        console.log(libraryBook);
-        myLibrary.push(libraryBook);
-        console.log(myLibrary);
-
-        /*
-        let newBook = document.createElement('div');
-        let newTitle = document.createElement('p');
-        newBook.classList.add('book');
-        newTitle.classList.add('title');
-        */
+        // Creates a book object from user input and alert missing information
+        if (bookTitle === '' || bookAuthor === '' || bookPages === '' || bookGenre === '') {
+            submitMessage.textContent = "Some information is missing.";
+            submitMessage.style.color = "red";
+        } else {
+            const libraryBook = new Book(bookTitle, bookAuthor, bookPages, bookGenre);
+            console.log(libraryBook);
+            myLibrary.push(libraryBook);
+            console.log(myLibrary);
+            submitMessage.style.color = "forestgreen";
+            submitMessage.textContent = "You have added a book to the library!";
+            let newBook = document.createElement('div');
+            let newTitle = document.createElement('p');
+            newBook.classList.add('book');
+            newTitle.classList.add('title');
+            newTitle.textContent = bookTitle;
+            firstShelf.appendChild(newBook);
+            newBook.appendChild(newTitle);
+        }
+        
     });
 };
 addBookToLibrary();
 
 
+// Open the book catalog submit form
+bookCatalog.addEventListener('click', () => {
+    bookModal.style.display = "flex";
+    bookcase.style.display = "none";
+});
 
-
-
-
-
-
-// "Book" object format:
-// const BookNickname = new Book('itle', 'author', 'pages', 'read');
-
-// Book example:
-// const PumpkinQueen = new Book('Long Live the Pumpkin Queen', 'Shea Ernshaw', '311', 'read part of');
-// console.log(PumpkinQueen.info());
-
+// Close the book modal
+closeButton.addEventListener('click', () => {
+    bookModal.style.display = "none";
+    submitMessage.textContent = "";
+    bookcase.style.display = "grid";
+})
 
 
