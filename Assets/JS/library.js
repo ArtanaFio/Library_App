ee2c95bc674a1f5f5dfa0513b8acbc3f05a7e5a0
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-05-13 16:17:37 -0400

    Clear input fields and prevent array duplicates
    Used find() array method to find any existing book objects in the array
    that are an exact match to the input values and used if statements to
    prevent adding the duplicate book to the myLibrary array and to inform
    the user of the incident. Also reorganized the JavaScript code; created
    the clearNotices and clearInput functions to allow different event
    listeners to perform the same functions as necessary.

--- Assets/JS/library.js
+++ Assets/JS/library.js
@@ -12,6 +12,7 @@ const submitButton = document.querySelector(".submit-button");
 
 const messageBox = document.querySelector(".messages");
 
+const allInput = document.querySelectorAll("input");
 const bookTitleInput = document.getElementById("book-title");
 const bookAuthorInput = document.getElementById("book-author");
 const bookPagesInput = document.getElementById("book-pages");
@@ -22,6 +23,11 @@ const firstShelf = document.querySelector(".first");
 const secondShelf = document.querySelector(".second");
 const thirdShelf = document.querySelector(".third");
 const fourthShelf = document.querySelector(".fourth");
+
+let bookTitle;
+let bookAuthor;
+let bookPages;
+let bookGenre;
 
 // Function to create a "book" object using an object constructor
 
@@ -37,6 +43,9 @@ function Book(title, author, pages, genre) {
     };
 }
 
+// if checkBook = newBook, don't add it
+// don't forget to commit about clearing inputs after successfully adding book
+
 // Function to transform any string input into a title case version of the string
 
 function titleCase(string) {
@@ -49,196 +58,217 @@ function titleCase(string) {
 
 // Function to add the "book" objects to the "myLibrary" array
 
-function addBookToLibrary() {
 
-    submitButton.addEventListener('click', () => {
-        
-        //remove old notifications
-        const oldMessages = messageBox.querySelectorAll("p");
 
-        oldMessages.forEach((notice) => {
-            notice.remove();
-        });
-        
+function clearInput() {
+    bookTitleInput.value = '';
+    bookAuthorInput.value = '';
+    bookPagesInput.value = '';
+    bookGenreOptions.selectedIndex = 0;
+};
 
-        const bookTitle = titleCase(bookTitleInput.value);
-        const bookAuthor = titleCase(bookAuthorInput.value);
-        const bookPages = bookPagesInput.value;
-        const bookGenre = bookGenreOptions.value;
+function clearNotices() {
+    //remove old notifications
+    const oldMessages = messageBox.querySelectorAll("p");
 
-        // Creates a book object from user input and alert missing information
+    oldMessages.forEach((notice) => {
+        notice.remove();
+    });
+};
 
-        if (bookTitle === '' || bookAuthor === '' || bookPages === '' || bookGenre === '') {
-            const missingMessage = document.createElement('p');
-            missingMessage.textContent = "Some information is missing.";
-            missingMessage.classList.add('error');
-            messageBox.appendChild(missingMessage);
-        } 
+submitButton.addEventListener('click', () => {
         
-        if (bookTitle === 'Title' || bookTitle === 'Book Title'|| bookTitle === 'A Title'|| bookTitle === 'Any Title') {
-            const titleMessage = document.createElement('p');
-            titleMessage.textContent = "Please enter a proper book title.";
-            titleMessage.classList.add('error');
-            messageBox.appendChild(titleMessage);
-        }
+    clearNotices();
         
-        if (bookAuthor === 'First & Last Name' || bookAuthor === 'Author' || bookAuthor === 'Author Name' || bookAuthor === 'First Last' ||  bookAuthor === 'First Name' ||  bookAuthor === 'Last Name') {
-            const authorMessage = document.createElement('p');
-            authorMessage.textContent = "Please enter a valid name.";
-            authorMessage.classList.add('error');
-            messageBox.appendChild(authorMessage);
-        }
+    const bookTitle = titleCase(bookTitleInput.value);
+    const bookAuthor = titleCase(bookAuthorInput.value);
+    const bookPages = bookPagesInput.value;
+    const bookGenre = bookGenreOptions.value;
+
+    let bookDuplicate = myLibrary.find(existingBook =>
+        existingBook.title === titleCase(bookTitleInput.value) && existingBook.author === titleCase(bookAuthorInput.value) && existingBook.pages === bookPagesInput.value && existingBook.genre === bookGenreOptions.value
+    );
+
+    if (bookDuplicate) {
+        const bookDuplicateMessage = document.createElement('p');
+        bookDuplicateMessage.textContent = "Sorry, the library already has this book. Please donate another book.";
+        bookDuplicateMessage.classList.add('error');
+        messageBox.appendChild(bookDuplicateMessage);
+    }
+
+    if (bookTitle === '' || bookAuthor === '' || bookPages === '' || bookGenre === '') {
+        const missingMessage = document.createElement('p');
+        missingMessage.textContent = "Some information is missing.";
+        missingMessage.classList.add('error');
+        messageBox.appendChild(missingMessage);
+    } 
         
-        if (bookPages === '') {
-            // do need to do anything
-        } else if (bookPages < 5) {
-            const pagesMessage = document.createElement('p');
-            pagesMessage.textContent = "Please make sure your book has at least 5 pages.";
-            pagesMessage.classList.add('error');
-            messageBox.appendChild(pagesMessage);
-        }
+    if (bookTitle === 'Title' || bookTitle === 'Book Title'|| bookTitle === 'A Title'|| bookTitle === 'Any Title') {
+        const titleMessage = document.createElement('p');
+        titleMessage.textContent = "Please enter a proper book title.";
+        titleMessage.classList.add('error');
+        messageBox.appendChild(titleMessage);
+    }
         
-        if ((bookTitle !== '' && bookAuthor !== '' && bookPages !== '' && bookGenre !== '') && (bookTitle !== 'Title' && bookTitle !== 'Book Title') && (bookAuthor !== 'First & Last Name' && bookAuthor !== 'Author' && bookAuthor !== 'Author Name' && bookAuthor !== 'First Last' &&  bookAuthor !== 'First Name' &&  bookAuthor !== 'Last Name') && (bookPages >= 5)) {
+    if (bookAuthor === 'First & Last Name' || bookAuthor === 'Author' || bookAuthor === 'Author Name' || bookAuthor === 'First Last' ||  bookAuthor === 'First Name' ||  bookAuthor === 'Last Name') {
+        const authorMessage = document.createElement('p');
+        authorMessage.textContent = "Please enter a valid name.";
+        authorMessage.classList.add('error');
+        messageBox.appendChild(authorMessage);
+    }
+        
+    if (bookPages < 5 && bookPages !== '') {
+        const pagesMessage = document.createElement('p');
+        pagesMessage.textContent = "Please make sure your book has at least 5 pages.";
+        pagesMessage.classList.add('error');
+        messageBox.appendChild(pagesMessage);
+    }
+        
+    if (!bookDuplicate && (bookTitle !== '' && bookAuthor !== '' && bookPages !== '' && bookGenre !== '') && (bookTitle !== 'Title' && bookTitle !== 'Book Title') && (bookAuthor !== 'First & Last Name' && bookAuthor !== 'Author' && bookAuthor !== 'Author Name' && bookAuthor !== 'First Last' &&  bookAuthor !== 'First Name' &&  bookAuthor !== 'Last Name') && (bookPages >= 5)) {
+        function addBookToLibrary() {
+            // Creates a book object from correct user input
             const libraryBook = new Book(bookTitle, bookAuthor, bookPages, bookGenre);
             myLibrary.push(libraryBook);
             console.log(myLibrary);
-            const successMessage = document.createElement('p');
-            successMessage.classList.add('correct');
-            successMessage.textContent = `You have added ${bookTitle} to the library!`;
-            messageBox.appendChild(successMessage);
+        };
+        addBookToLibrary();
+        const successMessage = document.createElement('p');
+        successMessage.classList.add('correct');
+        successMessage.textContent = `You have added ${bookTitle} to the library!`;
+        messageBox.appendChild(successMessage);
 
-            let newBook = document.createElement('div'); // Create visual representation of book
-            let newTitle = document.createElement('p');
-            newBook.classList.add('book');
-            newTitle.classList.add('title');
-            newTitle.textContent = bookTitle;
+        clearInput();
 
-            firstShelf.appendChild(newBook);
-            newBook.appendChild(newTitle);
+        allInput.forEach((input) => {
+            input.addEventListener('click', () => {
+                clearNotices();
+            });
+        })
 
-            let openBook = document.createElement('div');
-            openBook.classList.add('open-book-modal');
-
-            let closeBook = document.createElement('div');
-            closeBook.classList.add("close-box");
-
-            let closeBookButton = document.createElement('img');
-            closeBookButton.src = "Assets/Images/x-square.svg";
-            closeBookButton.classList.add("close-book");
-
-            let bookContent = document.createElement('div');
-            bookContent.classList.add('book-modal-content');
-
-            let blankSide = document.createElement('div');
-            blankSide.classList.add('blank-side');
-
-            let bookDetails = document.createElement('div');
-            bookDetails.classList.add('details');
-
-            let title = document.createElement('p');
-            title.classList.add('title-display');
-            title.textContent = `${bookTitle}`;
-
-            let author = document.createElement('p');
-            author.style.gridRow = "3 / 4";
-            author.style.alignSelf = "center";
-            author.textContent = `Written by ${bookAuthor}`;
-
-            let genre = document.createElement('p');
-            genre.style.gridRow = "4 / 5";
-            genre.style.alignSelf = "end";
-            genre.textContent = `Genre: ${bookGenre}`;
-
-            let pages = document.createElement('p');
-            pages.style.gridRow = "5 / 6";
-            pages.style.alignSelf = "start";
-            pages.textContent = `Number of Pages: ${bookPages}`;
-
-            switch (bookGenre) {
-                case 'Adventure':
-                    newBook.style.backgroundColor = "red";
-                    bookContent.style.borderColor = "red";
-                    break;
-                case 'Fantasy':
-                    newBook.style.backgroundColor = "mediumseagreen";
-                    bookContent.style.borderColor = "mediumseagreen";
-                    break;
-                case 'Mystery':
-                    newBook.style.backgroundColor = "indigo";
-                    newBook.style.color = "chocolate";
-                    bookContent.style.borderColor = "indigo";
-                    break;
-                case 'Horror & Thriller':
-                    newBook.style.backgroundColor = "midnightblue";
-                    newBook.style.color = "chocolate";
-                    bookContent.style.borderColor = "midnightblue";
-                    break;
-                case 'Science Fiction':
-                    newBook.style.backgroundColor = "darkviolet";
-                    bookContent.style.borderColor = "darkviolet";
-                    break;
-                case 'Historical Fiction':
-                    newBook.style.backgroundColor = "crimson";
-                    bookContent.style.borderColor = "crimson";
-                    break;
-                case 'Contemporary Fiction':
-                    newBook.style.backgroundColor = "aqua";
-                    bookContent.style.borderColor = "aqua";
-                    break;
-                case 'Romance':
-                    newBook.style.backgroundColor = "deeppink";
-                    bookContent.style.borderColor = "deeppink";
-                    break;
-                case 'Poetry':
-                    newBook.style.backgroundColor = "forestgreen";
-                    bookContent.style.borderColor = "forestgreen";
-                    break;
-                case 'Self-Help':
-                    newBook.style.backgroundColor = "mediumpurple";
-                    bookContent.style.borderColor = "mediumpurple";
-                    break;
-                case 'Biography & Memoirs':
-                    newBook.style.backgroundColor = "coral";
-                    bookContent.style.borderColor = "coral";
-                    break;
-                case 'Religion & Philosophy':
-                    newBook.style.backgroundColor = "sandybrown";
-                    bookContent.style.borderColor = "sandybrown";
-                    break;
-                case 'History':
-                    newBook.style.backgroundColor = "cornflowerblue";
-                    bookContent.style.borderColor = "cornflowerblue";
-                    break;
-                case 'Science & Technology':
-                    newBook.style.backgroundColor = "slateblue";
-                    bookContent.style.borderColor = "slateblue";
-                    break;
-            }
+        bookGenreOptions.addEventListener('click', () => {
+            clearNotices();
+        });
             
-            body.appendChild(openBook);
-            openBook.appendChild(bookContent);
-            openBook.appendChild(closeBook);
-            closeBook.appendChild(closeBookButton);
-            bookContent.appendChild(bookDetails);
-            bookDetails.appendChild(title);
-            bookDetails.appendChild(author);
-            bookDetails.appendChild(genre);
-            bookDetails.appendChild(pages);
+        let newBook = document.createElement('div'); // Create visual representation of book
+        let newTitle = document.createElement('p');
+        let openBook = document.createElement('div');
+        let closeBook = document.createElement('div');
+        let closeBookButton = document.createElement('img');
+        let bookContent = document.createElement('div');
+        let blankSide = document.createElement('div');
+        let bookDetails = document.createElement('div');
+        let title = document.createElement('p');
+        let author = document.createElement('p');
+        let genre = document.createElement('p');
+        let pages = document.createElement('p');
 
-            newBook.addEventListener('click', () => {
-                openBook.style.display = "flex";
-                bookcase.style.display = "none";
-            })
+        title.textContent = `${bookTitle}`;        
+        author.textContent = `Written by ${bookAuthor}`;
+        genre.textContent = `Genre: ${bookGenre}`;
+        pages.textContent = `Number of Pages: ${bookPages}`;
 
-            closeBookButton.addEventListener('click', () => {
-                openBook.style.display = "none";
-                bookcase.style.display = "grid";
-            })
-        }  
+        newBook.classList.add('book');
+        newTitle.classList.add('title');
+        newTitle.textContent = bookTitle;
+        openBook.classList.add('open-book-modal');
+        closeBook.classList.add("close-box");       
+        closeBookButton.src = "Assets/Images/x-square.svg";
+        closeBookButton.classList.add("close-book");        
+        bookContent.classList.add('book-modal-content');       
+        blankSide.classList.add('blank-side');        
+        bookDetails.classList.add('details');        
+        title.classList.add('title-display');
+        author.style.gridRow = "3 / 4";
+        author.style.alignSelf = "center";
+        genre.style.gridRow = "4 / 5";
+        genre.style.alignSelf = "end";
+        pages.style.gridRow = "5 / 6";
+        pages.style.alignSelf = "start";
+        switch (bookGenre) {
+            case 'Adventure':
+                newBook.style.backgroundColor = "red";
+                bookContent.style.borderColor = "red";
+                break;
+            case 'Fantasy':
+                newBook.style.backgroundColor = "mediumseagreen";
+                bookContent.style.borderColor = "mediumseagreen";
+                break;
+            case 'Mystery':
+                newBook.style.backgroundColor = "indigo";
+                newBook.style.color = "chocolate";
+                bookContent.style.borderColor = "indigo";
+                break;
+            case 'Horror & Thriller':
+                newBook.style.backgroundColor = "midnightblue";
+                newBook.style.color = "chocolate";
+                bookContent.style.borderColor = "midnightblue";
+                break;
+            case 'Science Fiction':
+                newBook.style.backgroundColor = "darkviolet";
+                bookContent.style.borderColor = "darkviolet";
+                break;
+            case 'Historical Fiction':
+                newBook.style.backgroundColor = "crimson";
+                bookContent.style.borderColor = "crimson";
+                break;
+            case 'Contemporary Fiction':
+                newBook.style.backgroundColor = "aqua";
+                bookContent.style.borderColor = "aqua";
+                break;
+            case 'Romance':
+                newBook.style.backgroundColor = "deeppink";
+                bookContent.style.borderColor = "deeppink";
+                break;
+            case 'Poetry':
+                newBook.style.backgroundColor = "forestgreen";
+                bookContent.style.borderColor = "forestgreen";
+                break;
+            case 'Self-Help':
+                newBook.style.backgroundColor = "mediumpurple";
+                bookContent.style.borderColor = "mediumpurple";
+                break;
+            case 'Biography & Memoirs':
+                newBook.style.backgroundColor = "coral";
+                bookContent.style.borderColor = "coral";
+                break;
+            case 'Religion & Philosophy':
+                newBook.style.backgroundColor = "sandybrown";
+                bookContent.style.borderColor = "sandybrown";
+                break;
+            case 'History':
+                newBook.style.backgroundColor = "cornflowerblue";
+                bookContent.style.borderColor = "cornflowerblue";
+                break;
+            case 'Science & Technology':
+                newBook.style.backgroundColor = "slateblue";
+                bookContent.style.borderColor = "slateblue";
+                break;
+        }
+            
+        firstShelf.appendChild(newBook);
+        newBook.appendChild(newTitle);
+        body.appendChild(openBook);
+        openBook.appendChild(bookContent);
+        openBook.appendChild(closeBook);
+        closeBook.appendChild(closeBookButton);
+        bookContent.appendChild(bookDetails);
+        bookDetails.appendChild(title);
+        bookDetails.appendChild(author);
+        bookDetails.appendChild(genre);
+        bookDetails.appendChild(pages);
 
-    });
-};
-addBookToLibrary();
+        newBook.addEventListener('click', () => {
+            openBook.style.display = "flex";
+            bookcase.style.display = "none";
+        })
+
+        closeBookButton.addEventListener('click', () => {
+            openBook.style.display = "none";
+            bookcase.style.display = "grid";
+        })
+    }  
+
+});
 
 
 // Open the book catalog submit form
@@ -250,8 +280,9 @@ bookCatalog.addEventListener('click', () => {
 // Close the book modal
 closeButton.addEventListener('click', () => {
     bookModal.style.display = "none";
-    //submitMessage.textContent = "";
     bookcase.style.display = "grid";
+    clearInput();
+    clearNotices();
     console.log("Books in the library:");
     console.log(myLibrary);
 })
