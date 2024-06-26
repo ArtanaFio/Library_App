6dcafb2c7041530e3eb083aa7fdbf751b96b43cd
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-06-26 17:45:07 -0400

    Create information modal
    Added a new modal to inform user how to use the app upon opening the
    app. Applied keyframes and setTimeout to display the information modal
    at the correct time and media queries for responsive design.

--- Assets/JS/open_app.js
+++ Assets/JS/open_app.js
@@ -1,5 +1,7 @@
 const appSign = document.getElementById("app-sign");
 const library = document.querySelector(".room");
+const directions = document.getElementById("info-modal");
+const closeDirectionsButton = document.querySelector(".info-close");
 
 setTimeout (() => {
     appSign.style.top = "40vh";
@@ -9,3 +11,11 @@ setTimeout (() => {
     appSign.style.textShadow = "5px 5px 2px darkgoldenrod";
     library.style.opacity = "1";
 }, 600);
+
+setTimeout (() => {
+    directions.style.opacity = "1";
+}, 1200);
+
+closeDirectionsButton.addEventListener("click", () => {
+    directions.style.display = "none";
+});
